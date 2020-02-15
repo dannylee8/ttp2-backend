ca2e4fde9792f5940dcc7aa2eed25c3d06a83ab7
Danny Lee <dreamer@panix.com>
2020-02-15 16:11:48 -0500

    Calculate portfolio value in backend and add value to User serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :cash
+  attributes :id, :username, :email, :cash, :portfolio_value
 
   has_many :stocks
+
+  def portfolio_value
+    total = 0
+    self.object.stocks.map do |stock|
+      total += stock.price * stock.shares
+    end 
+    return total
+  end 
+
 end
