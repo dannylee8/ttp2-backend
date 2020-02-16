44895b15fb52c7915ae37532fb1a1814aab49903
Danny Lee <dreamer@panix.com>
2020-02-15 21:24:43 -0500

    Add serializer methods for current portfolio value and stock current price

--- app/serializers/stock_serializer.rb
+++ app/serializers/stock_serializer.rb
@@ -1,5 +1,16 @@
 class StockSerializer < ActiveModel::Serializer
-  attributes :id, :symbol, :shares, :price
+  attributes :id, :symbol, :shares, :price, :current_price
 
   belongs_to :user
+
+  def current_price
+    # create IEX API client
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_f60d00f3b3774527b14ddc2510d54b18',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+      ) 
+      
+    # query API for each stocks price
+    return client.price(self.object.symbol)
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,14 +1,33 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :cash, :portfolio_value
+  attributes :id, :username, :email, :cash, :portfolio_cost, :portfolio_current_value
 
   has_many :stocks
 
-  def portfolio_value
+  def portfolio_cost
     total = 0
-    self.object.stocks.map do |stock|
+    self.object.stocks.each do |stock|
       total += stock.price * stock.shares
     end 
     return total
   end 
+
+  def portfolio_current_value
+    total = 0
+    # create IEX API client
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_f60d00f3b3774527b14ddc2510d54b18',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+      ) 
+    
+      
+    self.object.stocks.each do |stock|
+      price = client.price(stock.symbol)
+      total += price * stock.shares
+    end
+
+    return total
+    # query API for each stocks price
+  end
+
 
 end
