4bff0b7da4e233919fde2bf579b7114931b85ae6
Danny Lee <dreamer@panix.com>
2020-02-16 19:03:25 -0500

    Change API token to sandbox token for testing.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -14,9 +14,14 @@ class UserSerializer < ActiveModel::Serializer
   def portfolio_current_value
     total = 0
 
+    # client = IEX::Api::Client.new(
+    #   publishable_token: 'pk_9b7b0939edbc416e8ecee6a94c193697',
+    #   endpoint: 'https://cloud.iexapis.com/v1'
+    #   ) 
+
     client = IEX::Api::Client.new(
-      publishable_token: 'pk_9b7b0939edbc416e8ecee6a94c193697',
-      endpoint: 'https://cloud.iexapis.com/v1'
+      publishable_token: 'Tpk_f60d00f3b3774527b14ddc2510d54b18',
+      endpoint: 'https://sandbox.iexapis.com/v1'
       ) 
       
     self.object.stocks.each do |stock|
