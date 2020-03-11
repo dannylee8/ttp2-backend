72d62f46731d16ba2cc93e916cc9e196e1c91dcd
Danny Lee <dreamer@panix.com>
2020-03-10 20:31:43 -0400

    Add patch function to stocks_controller, if user already has a stock symbol.  patch shares to first object.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,32 @@
 class StocksController < ApplicationController
 
-  # create a stock 
+  # create a new stock or patch a stock if user id already has that symbol
   def create
-    @stock = Stock.new(stock_params)
-    if @stock.save
-      render json: {
-        status: :created,
-        stock: @stock
-      }
+    @stock = Stock.find_by(user_id: params[:user_id], symbol: params[:symbol])
+    if @stock
+      # render json: {
+        #   status: :found,
+        #   stock: @stock
+        # }
+        @stock.shares = @stock.shares + params[:shares].to_i;
+        @stock.save 
+        render json: {
+          status: :update,
+          stock: @stock
+        }
     else 
-      render json: {
-        status: 500,
-        errors: @stock.errors.full_messages
-      }
+      @stock = Stock.new(stock_params)
+      if @stock.save
+        render json: {
+          status: :created,
+          stock: @stock
+        }
+      else 
+        render json: {
+          status: 500,
+          errors: @stock.errors.full_messages
+        }
+      end
     end
   end
 
